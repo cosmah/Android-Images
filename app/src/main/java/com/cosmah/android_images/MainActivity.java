715a316a17bf4a4c49b0ae1855d5ae6eb18d3992
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-09-13 13:12:49 +0300

    images views

---
+++ app/src/main/java/com/cosmah/android_images/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cosmah.android_images;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
